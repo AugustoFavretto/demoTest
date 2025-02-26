fb4027d9cf12a1c51fde9adc54cbd5365e481544
Augusto Favretto <augusto.favretto@picpay.com>
2025-02-26 14:47:07 -0400

    Update port aplication

--- src/main/kotlin/com/ia/document/demo/MessageService.kt
+++ src/main/kotlin/com/ia/document/demo/MessageService.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service
 class MessageService(private val db: MessageRepository) {
     fun findMessages(): List<Message> = db.findAll().toList()
 
+
     fun findMessageById(id: String): Message? = db.findByIdOrNull(id)
 
     fun save(message: Message): Message = db.save(message)
